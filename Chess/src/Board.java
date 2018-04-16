c4d6d96f726bec3f53fc33b2ab3dac89c36bb069
tm44 <12568320+tm44rest@users.noreply.github.com>
2018-04-16 16:59:16 -0400

    Created window layout
    Board on the left, sidebar on the right.

--- Chess/src/Board.java
+++ Chess/src/Board.java
@@ -1,6 +1,7 @@
+import javax.swing.JPanel;
 
 /** An instance is a chess board. */
-public class Board {
+public class Board extends JPanel {
 
 	/** Constructor: creates an 8x8 grid of tiles with no pieces on it. */
 	public Board() {
--- Chess/src/Window.java
+++ Chess/src/Window.java
@@ -1,18 +1,51 @@
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 /**	An instance is a chess window. */
 public class Window extends JFrame {
 	private static final String windowTitle = "Chess";	// Title of the window
 	
+	private Board board;	// Chess board associated with this window
+	
+	// Chess players
+	private Player player1;
+	private Player player2;
+	
+	private JPanel sidebar;	// sidebar with information about the game such as
+							// turn #, time, score, and a reset button
+	
 	/**	Constructor: the main window of the chess game. */
 	public Window() {
 		super(windowTitle);
+		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+		
+		// Create the chess board
+		board = new Board();
+		
+		// Create the players
+		Player player1 = new Player(true);
+		Player player2 = new Player(false);
+		
+		// Create the sidebar
+		sidebar = createSidebar();
+		
+		// Add board and sidebar to the window
+		add(board);
+		add(sidebar);
 		
 		// Make window visible
 		pack();
 		setLocationRelativeTo(null);	// center the window on the screen
 		setVisible(true);
+	}
+	
+	/**	Create and return the sidebar. */
+	private JPanel createSidebar() {
+		// TODO: Implement sidebar
+		return new JPanel();
 	}
 	
 	/** Start the GUI */
