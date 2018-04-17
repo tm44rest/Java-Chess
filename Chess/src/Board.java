af52bc81994f23a7f2fd438bb7e0598b2a1ada03
tm44 <12568320+tm44rest@users.noreply.github.com>
2018-04-17 12:20:36 -0400

    Implemented the board
    Added TODO for the mouse event listener part of creating the tiles. But
    the board itself is visible.

--- Chess/src/Board.java
+++ Chess/src/Board.java
@@ -1,10 +1,37 @@
-import javax.swing.JPanel;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import java.util.ArrayList;
+import java.util.List;
 
+@SuppressWarnings("serial")
 /** An instance is a chess board. */
-public class Board extends JPanel {
+public class Board extends Box {
 
-	/** Constructor: creates an 8x8 grid of tiles with no pieces on it. */
+	private Tile[][] tiles;	// tile grid
+	
+	/** Constructor: creates an horizontal box of vertical boxes, each
+	 * 	containing 8 tiles with no pieces on them. */
 	public Board() {
+		super(BoxLayout.Y_AXIS);
+		
+		// Create the tile grid
+		tiles = new Tile[8][8];
+		
+		// inv: rows 0..i-1 have been set up
+		for (int i = 0; i != 8; i++) {
+			// Create the ith row of tiles
+			Box row = new Box(BoxLayout.X_AXIS);
+			
+			// inv: tiles 0..i-1 have been set up
+			for (int j = 0; j != 8; j++) {
+				// Create the tiles for each row
+				tiles[i][j] = new Tile(i,j);
+				row.add(tiles[i][j]);
+				// TODO: add a mouse event listener to the tile
+			}
+			
+			this.add(row);	// add the row of tiles to this board
+		}
 		
 	}
 }
--- Chess/src/Tile.java
+++ Chess/src/Tile.java
@@ -1,16 +1,27 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Point;
 
 /** An instance is a chess tile. 
  * 	Contains information for the tile's location, color, and what piece (if 
  * 	any) is on it. */
-public class Tile {
+public class Tile extends Canvas {
+	private static final int HEIGHT = 70;
+	private static final int WIDTH = 70;
+	
 	private Point location;	// the (x,y) location of this tile on the board
+	
+	private Piece piece;	// the piece on this tile (null if no piece)
 	
 	/** Constructor: A tile on the chess board. 
 	 * 	Point xy represents the (x,y) coordinate of the board.
 	 * 	i.e. (0,1) would be b8. */
 	public Tile (Point xy) {
 		location = xy;
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		piece = null;
 	}
 	
 	/** Constructor: A tile on the chess board. 
@@ -18,6 +29,24 @@ public class Tile {
 	 * 	i.e. (0,1) would be b8. */
 	public Tile(int x, int y) {
 		this(new Point(x,y));
+	}
+	
+	/** Update the piece on this tile. */
+	public void updatePiece(Piece p) {
+		piece = p;
+	}
+	
+	/** Paint this tile using g. */
+	public @Override void paint(Graphics g) {
+		// Paint the empty tile
+		if ((location.getX() + location.getY()) % 2 == 0) g.setColor(Color.white);
+		else g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH-1, HEIGHT-1);
+		
+		// Paint the piece on this tile
+		if (piece != null) {
+			// TODO: Paint the piece
+		}
 	}
 	
 }
