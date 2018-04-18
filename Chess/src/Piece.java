0a561ad565243315147d9ab25a7c9b8b002e1332
tm44 <12568320+tm44rest@users.noreply.github.com>
2018-04-18 11:52:18 -0400

    Began work on method tileMovement
    Created its framework, just need to implement the actual cases

--- Chess/src/Piece.java
+++ Chess/src/Piece.java
@@ -1,3 +1,6 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 /** An instance is a chess piece. */
 public class Piece {
@@ -9,5 +12,34 @@ public class Piece {
 	public Piece(boolean isWhite, PieceType type) {
 		this.isWhite = isWhite;
 		this.type = type;
+	}
+	
+	/** Return a list of possible points this piece can move to depending on its
+	 * 	type and the given location.
+	 * 	Precondition: Point xy is between (0,0) and (7,7) */
+	public List<Point> tileMovement(Point xy)  {
+		// TODO implement tile movement for this piece
+		List<Point> movementPoints = new ArrayList<Point>();
+		switch (type) {
+		case PAWN: break;
+		case KNIGHT: break;
+		case BISHOP: break;
+		case ROOK: break;
+		case QUEEN: break;
+		case KING: break;
+		}
+		removeInvalidPoints(movementPoints);
+		return movementPoints;
+	}
+	
+	/** Helper method to remove all points that are not between (0,0) and (7,7)*/
+	private void removeInvalidPoints(List<Point> list) {
+		for (Point xy : list) {
+			int x = (int) xy.getX();
+			int y = (int) xy.getY();
+			if (x > 7 || x < 0 || y > 7 || y < 0)  {
+				list.remove(xy);
+			}
+		}
 	}
 }
