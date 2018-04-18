0c0beee2bd435b9cd640e087ab6c007751ffc084
tm44 <12568320+tm44rest@users.noreply.github.com>
2018-04-18 11:51:38 -0400

    Implemented PieceType
    Picture files not actually placed in src yet

--- Chess/src/PieceType.java
+++ Chess/src/PieceType.java
@@ -1,5 +1,18 @@
 
 /** An enumeration of all possible chess piece types. */
 public enum PieceType {
-
+	PAWN	("p", "/pawn.png"),
+	KNIGHT	("n", "/knight.png"),
+	BISHOP	("B", "/bishop.png"),
+	ROOK	("r", "/rook.png"),
+	QUEEN	("q", "/queen.png"),
+	KING	("k", "/king.png");
+	
+	private final String abbreviation;	// its chess abbreviation
+	private final String picture;	// location of its picture
+	
+	PieceType(String abbreviation, String picture) {
+		this.abbreviation = abbreviation;
+		this.picture = picture;
+	}
 }
