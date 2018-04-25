d93aef2ea1554ba5fb40ec514e94352de34c2927
tm44 <12568320+tm44rest@users.noreply.github.com>
2018-04-25 12:10:10 -0400

    Bug fixed rookMovement and tested it.
    Need to clean up the method to be more readable using a helper method.

--- Chess/src/ChessTest.java
+++ Chess/src/ChessTest.java
@@ -1,12 +1,57 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Point;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 class ChessTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPieces() {
+		testRookMovement();
 	}
 
+	void testRookMovement() {
+		// No pieces in the way, center
+		Board board1 = new Board();
+		Piece rookPiece1 = new Piece(true, PieceType.ROOK, board1, new Point(4,4));
+		Set<Point> methodSet1 = rookPiece1.tileMovement();
+		Set<Point> manualSet1 = new HashSet<Point>();
+		manualSet1.add(new Point(4,0));	manualSet1.add(new Point(4,1));
+		manualSet1.add(new Point(4,2));	manualSet1.add(new Point(4,3));
+		manualSet1.add(new Point(4,5));	manualSet1.add(new Point(4,6));	
+		manualSet1.add(new Point(4,7));	manualSet1.add(new Point(0,4));
+		manualSet1.add(new Point(1,4));	manualSet1.add(new Point(2,4));
+		manualSet1.add(new Point(3,4));	manualSet1.add(new Point(5,4));
+		manualSet1.add(new Point(6,4));	manualSet1.add(new Point(7,4));
+		assertEquals(true, manualSet1.equals(methodSet1));
+		
+		// No pieces in the way, corner
+		Board board2 = new Board();
+		Piece rookPiece2 = new Piece(false, PieceType.ROOK, board2, new Point(7,0));
+		Set<Point> methodSet2 = rookPiece2.tileMovement();
+		Set<Point> manualSet2 = new HashSet<Point>();
+		manualSet2.add(new Point(0,0));	manualSet2.add(new Point(1,0));
+		manualSet2.add(new Point(2,0));	manualSet2.add(new Point(3,0));
+		manualSet2.add(new Point(4,0));	manualSet2.add(new Point(5,0));
+		manualSet2.add(new Point(6,0));	manualSet2.add(new Point(7,1));
+		manualSet2.add(new Point(7,2));	manualSet2.add(new Point(7,3));
+		manualSet2.add(new Point(7,4));	manualSet2.add(new Point(7,5));
+		manualSet2.add(new Point(7,6));	manualSet2.add(new Point(7,7));
+		assertEquals(true, manualSet2.equals(methodSet2));
+		
+		// Pieces in the way, corner
+		Board board3 = new Board();
+		Piece whiteRook = new Piece(true, PieceType.ROOK, board3, new Point(7,7));
+		Piece whiteBishop = new Piece(true, PieceType.BISHOP, board3, new Point(5,7));
+		Piece blackPawn = new Piece(false, PieceType.PAWN, board3, new Point(7,2));
+		Set<Point> methodSet3 = whiteRook.tileMovement();
+		Set<Point> manualSet3 = new HashSet<Point>();
+		manualSet3.add(new Point(6,7));	manualSet3.add(new Point(7,6));
+		manualSet3.add(new Point(7,5));	manualSet3.add(new Point(7,4));
+		manualSet3.add(new Point(7,3));	manualSet3.add(new Point(7,2));
+		assertEquals(true, manualSet3.equals(methodSet3));
+	}
 }
--- Chess/src/Piece.java
+++ Chess/src/Piece.java
@@ -1,19 +1,24 @@
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /** An instance is a chess piece. */
 public class Piece {
 	private boolean isWhite;	// "this piece is white"
 	private PieceType type;		// the type of piece this is i.e. pawn, knight etc.
 	private Board board;	// the board this piece is on
+	private Point location;	// the location of this piece on board
 	
 	/** Constructor: creates a chess piece with specified color
-	 * 	and piece type. */
-	public Piece(boolean isWhite, PieceType type, Board board) {
+	 * 	and piece type at a certain location on the board. */
+	public Piece(boolean isWhite, PieceType type, Board board, Point location) {
 		this.isWhite = isWhite;
 		this.type = type;
 		this.board = board;
+		updateLocation(location);	// updates both this field and the
+									// corresponding tile's field
 	}
 	
 	/** Return this piece's type. */
@@ -26,21 +31,23 @@ public class Piece {
 		return isWhite;
 	}
 	
-	/** Return a list of possible points this piece can move to depending on its
-	 * 	type and the given location.
-	 * 	Precondition: Point xy is between (0,0) and (7,7) */
-	public List<Point> tileMovement(Point xy)  {
+	/** Update the location of this piece on the board. */
+	public void updateLocation(Point xy) {
+		board.getTile((int) xy.getX(), (int) xy.getY()).updatePiece(this);;
+		location = xy;
+	}
+	
+	/** Return a set of possible points this piece can move to depending on its
+	 * 	type and the current location. */
+	public Set<Point> tileMovement()  {
 		// TODO implement tile movement for this piece
-		List<Point> movementPoints = new ArrayList<Point>();
+		Set<Point> movementPoints = new HashSet<Point>();
 		switch (type) {
 		case PAWN: break;
 		case KNIGHT: break;
 		case BISHOP: break;
 		case ROOK: {
-			movementPoints.addAll(rookMovement(xy,true,true));	// Right
-			movementPoints.addAll(rookMovement(xy,false,true));	// Left
-			movementPoints.addAll(rookMovement(xy,true,false));	// Forward
-			movementPoints.addAll(rookMovement(xy,false,false));// Behind	
+			movementPoints.addAll(rookMovement(location,board,isWhite));
 			break;
 		}
 		case QUEEN: break;
@@ -50,19 +57,21 @@ public class Piece {
 	}
 	
 	/** Return false iff 0 <= x <= 7 and 0 <= y <= 7. */
-	private boolean isOutOfBounds(int x, int y) {
+	private static boolean isOutOfBounds(int x, int y) {
 		return !(x >= 0 && x <= 7 && y >= 0 && y <= 7);
 	}
 	
-	/** Return a list of all the points the rook can go to starting at xy and
-	 * 	moving positively if positive and negatively if not positive along 
-	 * 	the x-axis if isX and y-axis if not isX. */
-	private ArrayList<Point> rookMovement(Point xy, boolean positive, boolean isX) {
-		ArrayList<Point> movementPoints = new ArrayList<Point>();
+	/** Return a list of all the points the rook can go to starting at xy along 
+	 *  board and interacting with pieces according to its color isWhite. */
+	private static HashSet<Point> rookMovement(Point xy, Board board, boolean isWhite) {
+		// TODO: Clean up this method (add a helper)
+		HashSet<Point> movementPoints = new HashSet<Point>();
+		
+		// Points to the east of the rook
 		boolean mustStop = false;
 		int x = (int) xy.getX();
 		int y = (int) xy.getY();
-		while (!mustStop) {
+		while (!mustStop) {	
 			x = x+1;
 			if (isOutOfBounds(x,y)) mustStop = true;
 			else {
@@ -72,6 +81,52 @@ public class Piece {
 				else {movementPoints.add(new Point(x,y)); mustStop = true;}
 			}
 		}
+		
+		// Points to the south of the rook
+		mustStop = false;
+		x = (int) xy.getX();
+		y = (int) xy.getY();
+		while (!mustStop) {
+			y = y+1;
+			if (isOutOfBounds(x,y)) mustStop = true;
+			else {
+				Piece checkedPiece = board.getTile(x,y).getPiece();
+				if (checkedPiece == null) {movementPoints.add(new Point(x,y));}
+				else if (checkedPiece.isWhite() == isWhite) mustStop = true;
+				else {movementPoints.add(new Point(x,y)); mustStop = true;}
+			}
+		}
+		
+		// Points to the west of the rook
+		mustStop = false;
+		x = (int) xy.getX();
+		y = (int) xy.getY();
+		while (!mustStop) {
+			x = x-1;
+			if (isOutOfBounds(x,y)) mustStop = true;
+			else {
+				Piece checkedPiece = board.getTile(x,y).getPiece();
+				if (checkedPiece == null) {movementPoints.add(new Point(x,y));}
+				else if (checkedPiece.isWhite() == isWhite) mustStop = true;
+				else {movementPoints.add(new Point(x,y)); mustStop = true;}
+			}
+		}
+		
+		// Points to the north of the rook
+		mustStop = false;
+		x = (int) xy.getX();
+		y = (int) xy.getY();
+		while (!mustStop) {	
+			y = y-1;
+			if (isOutOfBounds(x,y)) mustStop = true;
+			else {
+				Piece checkedPiece = board.getTile(x,y).getPiece();
+				if (checkedPiece == null) {movementPoints.add(new Point(x,y));}
+				else if (checkedPiece.isWhite() == isWhite) mustStop = true;
+				else {movementPoints.add(new Point(x,y)); mustStop = true;}
+			}
+		}
+		
 		return movementPoints;
 	}
 }
